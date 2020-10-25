9c6468d7d1ffb92125b2d70884b8857afded6105
danielletang <danielletang1229@gmail.com>
2020-10-26 01:25:44 +1100

    upload

---
+++ app/src/main/java/edu/stanford/danielletang/mymaps/models/Place.kt
@@ -0,0 +1,8 @@
+package edu.stanford.danielletang.mymaps.models
+
+import java.io.Serializable
+
+data class Place(
+    val title: String, val description: String,
+    val latitude: Double, val longitude: Double,
+) : Serializable
